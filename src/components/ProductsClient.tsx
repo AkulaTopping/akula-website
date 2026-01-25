d7ac6b3dada3d49af3be9ecadfe99e4f5c99eb33
kisk <tarekanwer23@yahoo.com>
2026-01-25 19:22:18 +0200

    deploy

--- src/components/ProductsClient.tsx
+++ src/components/ProductsClient.tsx
@@ -37,6 +37,7 @@ export default function ProductsClient({ products }: { products: Product[] }) {
 
     window.open(`https://wa.me/201011286690?text=${message}`, "_blank");
   };
+  
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
